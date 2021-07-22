2bb6e0d780eec8789f3bfb5f3621ec96974ca435
garikdallari <garikdallari@gmail.com>
2021-07-22 23:19:12 +0300

    produkts+aktualitates+footer

---
+++ src/js/map.js
@@ -0,0 +1,8 @@
+// var map = L.map('map').setView([0, 0], 2);
+
+//   L.esri.basemapLayer('Streets').addTo(map);
+
+//   //  (new constructor syntax at 2.0.0)
+//   L.esri.Cluster.featureLayer({
+//     url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Earthquakes_Since1970/MapServer/0'
+//   }).addTo(map);
